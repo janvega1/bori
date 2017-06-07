dea186f60e330d208e116ae612c579c72695bc14
janvega1 <jan.vega1@upr.edu>
2017-06-07 13:36:57 -0400

    Register file renaming; fase 1

--- ArquiProject_JJL.v
+++ ArquiProject_JJL.v
@@ -298,13 +298,13 @@ Sign_Extension2 signExt2 (SignExt_Out2, Mc_Out, dataType, SignExtEn2);
 
 Shifter shift (Shf_Out, Flags [2], Ma_Out, wireIR[6:5], Md_Out, SHFen);
 
-register_32_bits InsReg (wireIR, MemOut, IREn, clr, clk);
+register_32bits InsReg (wireIR, MemOut, IREn, clr, clk);
 
-register_32_bits StaReg (tempStaReg, Flags, StatusRegEn, clr, clk);
+register_32bits StaReg (tempStaReg, Flags, StatusRegEn, clr, clk);
 
-register_32_bits Mar (MAR_Out, ALU_Out, MAREn, clr, clk);
+register_32bits Mar (MAR_Out, ALU_Out, MAREn, clr, clk);
 
-register_32_bits Mdr (MDR_Out, SignExt_Out2, MDREn, clr, clk);
+register_32bits Mdr (MDR_Out, SignExt_Out2, MDREn, clr, clk);
 
 Ram ram (MDR_Out, MemEN, r_w, mfa, dataType, dwp, MAR_Out [7:0], tempMfc, MemOut);
 
@@ -1443,18 +1443,16 @@ endmodule
 
 
 //--------------------------------------------------------
-//				Register FIle
+//				Register File
 //--------------------------------------------------------
-module register_file (output reg [31:0] outA, outB, input [31:0] dataIn, input [3:0] readA, readB, writeA, input enable, clr, clk);
+module register_file (output reg [31:0] PA, PB, input [31:0] PC, input [3:0] readA, readB, writeA, input enable, clr, clk);
 
 reg decoder_enable;
 
-wire [31:0] temp;
-wire [31:0] temp1;
-wire [15:0] decoder_out;
-wire [31:0] register_out0, register_out1, register_out2, register_out3, register_out4, register_out5, register_out6, register_out7,
-            register_out8, register_out9, register_out10, register_out11, register_out12, register_out13, register_out14, register_out15;
-
+wire [31:0] tempA;
+wire [31:0] tempB;
+wire [15:0] dec_out;
+wire [31:0] R0_out,R1_out,R2_out,R3_out,R4_out,R5_out,R6_out,R7_out,R8_out,R9_out,R10_out,R11_out,R12_out,R13_out,R14_out,R15_out;
 always @ (enable)
 begin 
 	
@@ -1478,39 +1476,32 @@ end
 
 always @ (*)
 begin
-	outA = temp;
-	outB = temp1;
+	PA = tempA;
+	PB = tempB;
 end
      
 
-decoder dec (decoder_out, writeA, decoder_enable);
+decoder dec (dec_out, writeA, decoder_enable);
 
-register_32_bits register0 (register_out0, dataIn, ~decoder_out[0], clr, clk);
-register_32_bits register1 (register_out1, dataIn, ~decoder_out[1], clr, clk);
-register_32_bits register2 (register_out2, dataIn, ~decoder_out[2], clr, clk);
-register_32_bits register3 (register_out3, dataIn, ~decoder_out[3], clr, clk);
-register_32_bits register4 (register_out4, dataIn, ~decoder_out[4], clr, clk);
-register_32_bits register5 (register_out5, dataIn, ~decoder_out[5], clr, clk);
-register_32_bits register6 (register_out6, dataIn, ~decoder_out[6], clr, clk);
-register_32_bits register7 (register_out7, dataIn, ~decoder_out[7], clr, clk);
-register_32_bits register8 (register_out8, dataIn, ~decoder_out[8], clr, clk);
-register_32_bits register9 (register_out9, dataIn, ~decoder_out[9], clr, clk);
-register_32_bits register10 (register_out10, dataIn, ~decoder_out[10], clr, clk);
-register_32_bits register11 (register_out11, dataIn, ~decoder_out[11], clr, clk);
-register_32_bits register12 (register_out12, dataIn, ~decoder_out[12], clr, clk);
-register_32_bits register13 (register_out13, dataIn, ~decoder_out[13], clr, clk);
-register_32_bits register14 (register_out14, dataIn, ~decoder_out[14], clr, clk);
-register_32_bits register15 (register_out15, dataIn, ~decoder_out[15], clr, clk);
+register_32bits register0  (R0_out, PC, ~dec_out[0], clr, clk);
+register_32bits register1  (R1_out, PC, ~dec_out[1], clr, clk);
+register_32bits register2  (R2_out, PC, ~dec_out[2], clr, clk);
+register_32bits register3  (R3_out, PC, ~dec_out[3], clr, clk);
+register_32bits register4  (R4_out, PC, ~dec_out[4], clr, clk);
+register_32bits register5  (R5_out, PC, ~dec_out[5], clr, clk);
+register_32bits register6  (R6_out, PC, ~dec_out[6], clr, clk);
+register_32bits register7  (R7_out, PC, ~dec_out[7], clr, clk);
+register_32bits register8  (R8_out, PC, ~dec_out[8], clr, clk);
+register_32bits register9  (R9_out, PC, ~dec_out[9], clr, clk);
+register_32bits register10 (R10_out, PC, ~dec_out[10], clr, clk);
+register_32bits register11 (R11_out, PC, ~dec_out[11], clr, clk);
+register_32bits register12 (R12_out, PC, ~dec_out[12], clr, clk);
+register_32bits register13 (R13_out, PC, ~dec_out[13], clr, clk);
+register_32bits register14 (R14_out, PC, ~dec_out[14], clr, clk);
+register_32bits register15 (R15_out, PC, ~dec_out[15], clr, clk);
 
-
-
-
-
-mux_16to1 muxA (temp, readA, register_out0, register_out1, register_out2, register_out3, register_out4, register_out5, register_out6, register_out7,
-               register_out8, register_out9, register_out10, register_out11, register_out12, register_out13, register_out14, register_out15);
-
-mux_16to1 muxB (temp1, readB, register_out0, register_out1, register_out2, register_out3, register_out4, register_out5, register_out6, register_out7,
-               register_out8, register_out9, register_out10, register_out11, register_out12, register_out13, register_out14, register_out15);
+mux_16to1 muxA (tempA, readA, R0_out, R1_out, R2_out, R3_out, R4_out, R5_out, R6_out, R7_out, R8_out, R9_out, R10_out, R11_out, R12_out, R13_out, R14_out, R15_out);
+mux_16to1 muxB (tempB, readB, R0_out, R1_out, R2_out, R3_out, R4_out, R5_out, R6_out, R7_out, R8_out, R9_out, R10_out, R11_out, R12_out, R13_out, R14_out, R15_out);
 
 endmodule
 
@@ -1518,7 +1509,7 @@ endmodule
 //--------------------------------------------------------------------
 //					Register of 32 bits
 //--------------------------------------------------------------------
-module register_32_bits (output reg [31:0] Y, input [31:0] I,
+module register_32bits (output reg [31:0] Y, input [31:0] I,
 			 input LE, CLR, CLK);
 
 always @ (posedge CLK, negedge CLR)
