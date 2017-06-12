cae532c3c54f990dd25b1d66f4d03dd1e0045b1f
Jesse Villafane <jesse.villafane@upr.edu>
2017-06-12 11:49:35 -0400

    Cambios al ram

--- ArquiProject_JJL.v
+++ ArquiProject_JJL.v
@@ -1225,132 +1225,147 @@ out <= pipeline_in;
 
 endmodule
 
+
 //--------------------------------------------------------
 //				RAM 256
 //--------------------------------------------------------
 module Ram (
-	input [31:0] data_in   , 
+	input [31:0] input_data   , 
 	input enable,
-	input r_w , 
-	input mfa,
-	input [1:0] dtype,
-	input _dwp1, 
-	input [7:0] addr,
-	output reg mfc,
-	output reg [31:0] data_out         
+	input read_write , 
+	input memory_active,
+	input [1:0] word_type,
+	input double_div, 
+	input [7:0] address,
+	output reg memory_complete,
+	output reg [31:0] output_data         
 	);
-
-
 
 
 reg [7:0] ram[0:255];
 
-reg [7:0] t_addr;
+reg [7:0] temp_address;
 
-// constants
+// definitions
 parameter BYTE = 2'b00;
-parameter HALF = 2'b01;
+parameter HALF_WORD = 2'b01;
 parameter WORD = 2'b10;
-parameter DWORD = 2'b11;
+parameter DOUBLE_WORD = 2'b11;
 parameter WRITE = 1'b1;
 parameter READ = 1'b0;
 parameter ENABLE = 1'b0;
 
 
 
-always@(mfa)
+always@(memory_active)
 begin
+
+
+
 if(enable == 0) begin
-case(dtype)
-BYTE:begin
-if(r_w==WRITE)begin
-ram[addr] <= data_in[7:0];
-end
 
-data_out <= {{24{1'b0}}, ram[addr]};
-end
-HALF:begin
-t_addr[7:1] <= addr[7:1];
-t_addr[0] <= 1'b0;
-#5;
-if(r_w==WRITE)begin
-ram[addr] <= data_in[15:8];
-ram[addr+1] <= data_in[7:0];
-#3;
-end
+case(word_type)
 
-data_out <= {{16{1'b0}}, ram[addr], ram[addr+1] };
-#3;
-end
-WORD:begin
-t_addr[7:2] <= addr[7:2];
-t_addr[1:0] <= 2'b00;
-#1;
-
-if(r_w==WRITE)begin
-ram[t_addr] <= data_in[31:24];
-ram[t_addr+1] <= data_in[23:16];
-ram[t_addr+2] <= data_in[15:8];
-ram[t_addr+3] <= data_in[7:0];
-#1;
-end 
-
-data_out[31:24] <= ram[t_addr];
-data_out[23:16] <= ram[t_addr+1];
-data_out[15:8] <= ram[t_addr+2];
-data_out[7:0] <= ram[t_addr+3];
-#1;
-end
-DWORD:begin
-t_addr[7:3] <= addr[7:3];
-t_addr[2:0] <= 3'b000;
-#5;
-
-if(r_w==WRITE)
+BYTE:
 begin
-if(_dwp1)
-begin
-ram[t_addr] <= data_in[31:24];
-ram[t_addr+1] <= data_in[23:16];
-ram[t_addr+2] <= data_in[15:8];
-ram[t_addr+3] <= data_in[7:0];
-end
-else
-begin
-ram[t_addr+4] <= data_in[31:24];
-ram[t_addr+5] <= data_in[23:16];
-ram[t_addr+6] <= data_in[15:8];
-ram[t_addr+7] <= data_in[7:0];
-end
-#3;
-end 
+	if(read_write==WRITE)
+		begin
+			ram[address] <= input_data[7:0];
+		end
 
-if(_dwp1)
+		output_data <= {{24{1'b0}}, ram[address]};
+end
+
+HALF_WORD:
 begin
-data_out[31:24] <= ram[t_addr];
-data_out[23:16] <= ram[t_addr+1];
-data_out[15:8] <= ram[t_addr+2];
-data_out[7:0] <= ram[t_addr+3];
+	temp_address[7:1] <= address[7:1];
+	temp_address[0] <= 1'b0;
+	#5;
+
+	if(read_write==WRITE)
+	begin
+		ram[address] <= input_data[15:8];
+		ram[address+1] <= input_data[7:0];
+		#3;
+	end
+
+	output_data <= {{16{1'b0}}, ram[address], ram[address+1] };
+	#3;
 end
-else
+
+WORD:
 begin
-data_out[31:24] <= ram[t_addr+4];
-data_out[23:16] <= ram[t_addr+5];
-data_out[15:8] <= ram[t_addr+6];
-data_out[7:0] <= ram[t_addr+7];
+	temp_address[7:2] <= address[7:2];
+	temp_address[1:0] <= 2'b00;
+	#1;
+
+	if(read_write==WRITE)
+	begin
+		ram[temp_address] <= input_data[31:24];
+		ram[temp_address+1] <= input_data[23:16];
+		ram[temp_address+2] <= input_data[15:8];
+		ram[temp_address+3] <= input_data[7:0];
+		#1;
+	end 
+
+	output_data[31:24] <= ram[temp_address];
+	output_data[23:16] <= ram[temp_address+1];
+	output_data[15:8] <= ram[temp_address+2];
+	output_data[7:0] <= ram[temp_address+3];
+	#1;
 end
-#3;
-end
+
+DOUBLE_WORD:
+begin
+	temp_address[7:3] <= address[7:3];
+	temp_address[2:0] <= 3'b000;
+	#5;
+
+	if(read_write==WRITE)
+	begin
+		if(double_div)
+		begin
+			ram[temp_address] <= input_data[31:24];
+			ram[temp_address+1] <= input_data[23:16];
+			ram[temp_address+2] <= input_data[15:8];
+			ram[temp_address+3] <= input_data[7:0];
+		end
+		else
+		begin
+			ram[temp_address+4] <= input_data[31:24];
+			ram[temp_address+5] <= input_data[23:16];
+			ram[temp_address+6] <= input_data[15:8];
+			ram[temp_address+7] <= input_data[7:0];
+		end
+		#3;
+	end 
+
+	if(double_div)
+	begin
+		output_data[31:24] <= ram[temp_address];
+		output_data[23:16] <= ram[temp_address+1];
+		output_data[15:8] <= ram[temp_address+2];
+		output_data[7:0] <= ram[temp_address+3];
+	end
+	else
+	begin
+		output_data[31:24] <= ram[temp_address+4];
+		output_data[23:16] <= ram[temp_address+5];
+		output_data[15:8] <= ram[temp_address+6];
+		output_data[7:0] <= ram[temp_address+7];
+	end
+	#3;
+	end
 endcase
 
-#5 mfc <= 1'b1;
+#5 memory_complete <= 1'b1;
 end
 end
 
-always@(mfa)
+always@(memory_active)
 begin
-if (mfa == 1'b1)
-mfc <= 1'b0;
+if (memory_active == 1'b1)
+memory_complete <= 1'b0;
 end
 
 endmodule
